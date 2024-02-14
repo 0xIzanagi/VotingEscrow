46c4c6dd798fbfb9331af6d31d07a5f28d4b37c1
0xIzanagi <0x01Izanagi@proton.me>
2024-02-14 15:51:12 -0600

    update(vault): update ERC404Vault

--- src/ERC404Vault.sol
+++ src/ERC404Vault.sol
@@ -1,12 +1,274 @@
-// SPDX-License-Identifier: MIT
+/// @notice SPDX-License-Identifier: MIT
+/// @notice ERC404 Vault for yield bearing applications similar to ERC4626
+/// @dev this version takes an ERC404 as the asset
+/// @author Koto Protocol
 
 pragma solidity ^0.8.0;
 
 import {ERC404} from "./ERC404.sol";
+import {IERC404Vault} from "./interfaces/IERC404Vault.sol";
+import {Math} from "./utils/math.sol";
+import {Context} from "./utils/Context.sol";
 
-contract ERC404Vault is ERC404 {
+contract ERC404Vault is Context, ERC404, IERC404Vault {
+    using Math for uint256;
+    /**
+     * @dev Attempted to deposit more assets than the max amount for `receiver`.
+     */
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC404(_name, _symbol, _decimals) {}
+    error ERC404ExceededMaxDeposit(address receiver, uint256 assets, uint256 max);
+
+    /**
+     * @dev Attempted to mint more shares than the max amount for `receiver`.
+     */
+    error ERC404ExceededMaxMint(address receiver, uint256 shares, uint256 max);
+
+    /**
+     * @dev Attempted to withdraw more assets than the max amount for `receiver`.
+     */
+    error ERC404ExceededMaxWithdraw(address owner, uint256 assets, uint256 max);
+
+    /**
+     * @dev Attempted to redeem more shares than the max amount for `receiver`.
+     */
+    error ERC404ExceededMaxRedeem(address owner, uint256 shares, uint256 max);
+
+    ERC404 private immutable _asset;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, ERC404 asset_)
+        ERC404(_name, _symbol, _decimals)
+    {
+        _asset = asset_;
+    }
 
     function tokenURI(uint256 id_) public view override returns (string memory) {}
+
+    /**
+     * @dev See {IERC4626-asset}.
+     */
+    function asset() public view virtual returns (address) {
+        return address(_asset);
+    }
+
+    /**
+     * @dev See {IERC4626-totalAssets}.
+     */
+    function totalAssets() public view virtual returns (uint256) {
+        return _asset.erc20BalanceOf(address(this));
+    }
+
+    /**
+     * @dev See {IERC4626-convertToShares}.
+     */
+    function convertToShares(uint256 assets) public view virtual returns (uint256) {
+        return _convertToShares(assets, Math.Rounding.Floor);
+    }
+
+    /**
+     * @dev See {IERC4626-convertToAssets}.
+     */
+    function convertToAssets(uint256 shares) public view virtual returns (uint256) {
+        return _convertToAssets(shares, Math.Rounding.Floor);
+    }
+
+    /**
+     * @dev See {IERC4626-maxDeposit}.
+     */
+    function maxDeposit(address) public view virtual returns (uint256) {
+        return type(uint256).max;
+    }
+
+    /**
+     * @dev See {IERC4626-maxMint}.
+     */
+    function maxMint(address) public view virtual returns (uint256) {
+        return type(uint256).max;
+    }
+
+    /**
+     * @dev See {IERC4626-maxWithdraw}.
+     */
+    function maxWithdraw(address owner) public view virtual returns (uint256) {
+        return _convertToAssets(balanceOf[owner], Math.Rounding.Floor);
+    }
+
+    /**
+     * @dev See {IERC4626-maxRedeem}.
+     */
+    function maxRedeem(address owner) public view virtual returns (uint256) {
+        return balanceOf[owner];
+    }
+
+    /**
+     * @dev See {IERC4626-previewDeposit}.
+     */
+    function previewDeposit(uint256 assets) public view virtual returns (uint256) {
+        return _convertToShares(assets, Math.Rounding.Floor);
+    }
+
+    /**
+     * @dev See {IERC4626-previewMint}.
+     */
+    function previewMint(uint256 shares) public view virtual returns (uint256) {
+        return _convertToAssets(shares, Math.Rounding.Ceil);
+    }
+
+    /**
+     * @dev See {IERC4626-previewWithdraw}.
+     */
+    function previewWithdraw(uint256 assets) public view virtual returns (uint256) {
+        return _convertToShares(assets, Math.Rounding.Ceil);
+    }
+
+    /**
+     * @dev See {IERC4626-previewRedeem}.
+     */
+    function previewRedeem(uint256 shares) public view virtual returns (uint256) {
+        return _convertToAssets(shares, Math.Rounding.Floor);
+    }
+
+    /**
+     * @dev See {IERC4626-deposit}.
+     */
+    function deposit(uint256 assets, address receiver) public virtual returns (uint256) {
+        uint256 maxAssets = maxDeposit(receiver);
+        if (assets > maxAssets) {
+            revert ERC404ExceededMaxDeposit(receiver, assets, maxAssets);
+        }
+
+        uint256 shares = previewDeposit(assets);
+        _deposit(_msgSender(), receiver, assets, shares);
+
+        return shares;
+    }
+
+    function depositNFT(uint256 id, address receiver) public virtual returns (uint256) {
+        uint256 assets = _asset.units();
+        uint256 maxAssets = maxDeposit(receiver);
+        if (assets > maxAssets) {
+            revert ERC404ExceededMaxDeposit(receiver, assets, maxAssets);
+        }
+        
+        uint256 shares = previewDeposit(assets);
+        _depositNFT(_msgSender(), receiver, assets, shares, id);
+        return shares;
+    }
+
+    /**
+     * @dev See {IERC4626-mint}.
+     */
+    function mint(uint256 shares, address receiver) public virtual returns (uint256) {
+        uint256 maxShares = maxMint(receiver);
+        if (shares > maxShares) {
+            revert ERC404ExceededMaxMint(receiver, shares, maxShares);
+        }
+
+        uint256 assets = previewMint(shares);
+        _deposit(_msgSender(), receiver, assets, shares);
+
+        return assets;
+    }
+
+    /**
+     * @dev See {IERC4626-withdraw}.
+     */
+    function withdraw(uint256 assets, address receiver, address owner) public virtual returns (uint256) {
+        uint256 maxAssets = maxWithdraw(owner);
+        if (assets > maxAssets) {
+            revert ERC404ExceededMaxWithdraw(owner, assets, maxAssets);
+        }
+
+        uint256 shares = previewWithdraw(assets);
+        _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+        return shares;
+    }
+
+    /**
+     * @dev See {IERC4626-redeem}.
+     */
+    function redeem(uint256 shares, address receiver, address owner) public virtual returns (uint256) {
+        uint256 maxShares = maxRedeem(owner);
+        if (shares > maxShares) {
+            revert ERC404ExceededMaxRedeem(owner, shares, maxShares);
+        }
+
+        uint256 assets = previewRedeem(shares);
+        _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+        return assets;
+    }
+
+    /**
+     * @dev Internal conversion function (from assets to shares) with support for rounding direction.
+     */
+    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view virtual returns (uint256) {
+        return assets.mulDiv(totalSupply + 10 ** _decimalsOffset(), totalAssets() + 1, rounding);
+    }
+
+    /**
+     * @dev Internal conversion function (from shares to assets) with support for rounding direction.
+     */
+    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view virtual returns (uint256) {
+        return shares.mulDiv(totalAssets() + 1, totalSupply + 10 ** _decimalsOffset(), rounding);
+    }
+
+    /**
+     * @dev Deposit/mint common workflow.
+     */
+    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual {
+        // If _asset is ERC-777, `transferFrom` can trigger a reentrancy BEFORE the transfer happens through the
+        // `tokensToSend` hook. On the other hand, the `tokenReceived` hook, that is triggered after the transfer,
+        // calls the vault, which is assumed not malicious.
+        //
+        // Conclusion: we need to do the transfer before we mint so that any reentrancy would happen before the
+        // assets are transferred and before the shares are minted, which is a valid state.
+        // slither-disable-next-line reentrancy-no-eth
+        _asset.transferFrom(caller, address(this), assets);
+       //_mint(receiver, shares);
+
+        emit Deposit(caller, receiver, assets, shares);
+    }
+
+    function _depositNFT(address caller, address receiver, uint256 assets, uint256 shares, uint256 id) internal virtual {
+        _asset.transferFrom(caller, address(this), id);
+
+        emit DepositNFT(caller, receiver, assets, shares, id);
+    }
+
+    /**
+     * @dev Withdraw/redeem common workflow.
+     */
+    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares)
+        internal
+        virtual
+    {
+        if (caller != owner) {
+            _spendAllowance(owner, caller, shares);
+        }
+
+        // If _asset is ERC-777, `transfer` can trigger a reentrancy AFTER the transfer happens through the
+        // `tokensReceived` hook. On the other hand, the `tokensToSend` hook, that is triggered before the transfer,
+        // calls the vault, which is assumed not malicious.
+        //
+        // Conclusion: we need to do the transfer after the burn so that any reentrancy would happen after the
+        // shares are burned and after the assets are transferred, which is a valid state.
+        //_transferERC20WithERC721(owner, address(0), shares);
+        //_burn(owner, shares);
+        _asset.transfer(receiver, assets);
+
+        emit Withdraw(caller, receiver, owner, assets, shares);
+    }
+
+    function _spendAllowance(address _owner, address _spender, uint256 _shares) internal {
+        uint256 _allowance = allowance[_owner][_spender];
+        if (_allowance != type(uint256).max) {
+            if (_allowance < _shares) revert InsufficientAllowance();
+            allowance[_owner][_spender] = _allowance - _shares;
+        }
+    }
+
+    function _decimalsOffset() internal view virtual returns (uint8) {
+        return 0;
+    }
 }
--- src/libraries/ERC721Receiver.sol
+++ src/libraries/ERC721Receiver.sol
@@ -2,12 +2,7 @@
 pragma solidity ^0.8.0;
 
 abstract contract ERC721Receiver {
-  function onERC721Received(
-    address,
-    address,
-    uint256,
-    bytes calldata
-  ) external virtual returns (bytes4) {
-    return ERC721Receiver.onERC721Received.selector;
-  }
+    function onERC721Received(address, address, uint256, bytes calldata) external virtual returns (bytes4) {
+        return ERC721Receiver.onERC721Received.selector;
+    }
 }
